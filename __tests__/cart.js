7d6639730d52e18d9a074f63aadc9fe2f75a5537
oquirozm <omarquirozmijares@gmail.com>
2019-01-02 15:55:22 +0100

    Cleaned the project and fixed some methods
    - Removed the helpers.js and localStorage.js files. They weren't
    necessary anymore.
    - Fixed the updateItem method.
    - Changed the way I was finding the index of an item across all the
    program (replaced the old way for a one-liner conbination of map and
    indexOf.

--- __tests__/cart.js
+++ __tests__/cart.js
@@ -4,7 +4,7 @@
 |--------------------------------------------------------------------------
 */
 
-const { Cart, newCart } = require('../src/scripts/lib/cart');
+const Cart = require('../src/scripts/lib/cart');
 
 const demoCartItems = [
   {
@@ -30,58 +30,74 @@ const itemToAdd = {
   slug: 'item-4',
 };
 
-test('after initializing Cart an object is returned', () => {
-  expect(typeof new Cart()).toBe('object');
-});
+/*
+ * Tests for the Cart's initialization.
+ */
 test('after initializing newCart an object is returned', () => {
-  expect(typeof new newCart()).toBe('object');
+  expect(typeof new Cart()).toBe('object');
 });
 
 /*
- * Cart initializes correctly with passed down items.
+ * Tests for the .getAllItems method.
  */
 test('getAllItems should return an array of items', () => {
-  let Cart = new newCart(null, demoCartItems);
-  expect(Array.isArray(Cart.getAllItems())).toBe(true);
+  let cart = new Cart(null, demoCartItems);
+  expect(Array.isArray(cart.getAllItems())).toBe(true);
 });
 test('getAllItems should return an array equal to the array that was passed on initialization', () => {
-  let Cart = new newCart(null, demoCartItems);
-  expect(Cart.getAllItems()).toEqual(demoCartItems);
+  let cart = new Cart(null, demoCartItems);
+  expect(cart.getAllItems()).toEqual(demoCartItems);
 });
 
+/*
+ * Tests for the .addItem method.
+ */
 test('addItem should add an item to the cart', () => {
-  let Cart = new newCart(null, demoCartItems);
-  Cart.addItem(itemToAdd);
-  let cartItems = Cart.getAllItems();
+  let cart = new Cart(null, demoCartItems);
+  cart.addItem(itemToAdd);
+  let cartItems = cart.getAllItems();
   expect(cartItems.length).toBe(4);
   expect(cartItems[cartItems.length - 1]).toEqual(itemToAdd);
 });
 
+/*
+ * Tests for the .getItem method.
+ */
 test('getItem should return a single item object.', () => {
-  let Cart = new newCart(null, demoCartItems);
-  let item = Cart.getItem(2);
+  let cart = new Cart(null, demoCartItems);
+  let item = cart.getItem(2);
   expect(item.id).toBe(2);
   expect(item).toEqual(demoCartItems[2 - 1]);
 });
 
+/*
+ * Tests for the .removeItem method.
+ */
 test('removeItem should remove an item from the cart.', () => {
-  let Cart = new newCart(null, demoCartItems);
-  Cart.removeItem(2);
-  let items = Cart.getAllItems();
+  let cart = new Cart(null, demoCartItems);
+  cart.removeItem(2);
+  let items = cart.getAllItems();
   expect(items.length).toBe(2);
   expect(items).toEqual(
     expect.not.arrayContaining(demoCartItems.filter(item => item.id === 2))
   );
 });
 
+/*
+ * Tests for .updateItem method.
+ */
 test('updateItem should update correctly an item', () => {
-  let Cart = new newCart(null, demoCartItems);
-  Cart.updateItem(2, { name: 'Neil Young Concert' });
-  let item = Cart.getItem(2);
+  let cart = new Cart(null, demoCartItems);
+  cart.updateItem(2, { name: 'Neil Young Concert' });
+  let item = cart.getItem(2);
   expect(item.name).toBe('Neil Young Concert');
 });
+
+/*
+ * Tests for .clear method
+ */
 test('clear should set the items to an empty array.', () => {
-  let Cart = new newCart(null, demoCartItems);
-  Cart.clear();
-  expect(Cart.getAllItems()).toEqual([]);
+  let cart = new Cart(null, demoCartItems);
+  cart.clear();
+  expect(cart.getAllItems()).toEqual([]);
 });
--- src/scripts/lib/cart.js
+++ src/scripts/lib/cart.js
@@ -8,27 +8,33 @@
 | An item of the cart should be an object with the following properties:
 | item.id:int, item.name:str, item.slug:str, item.type:str
 */
-function newCart(storage, initItems) {
+function Cart(storage, initItems) {
+  // Private property that stores the item objects in an array.
   this._items = [];
 
+  // Save items to the storage method.
   this.saveItems = (cartItems = []) => {
     this._items = cartItems;
     storage.save(this._items);
   };
 
+  // Retrieve all the cart items.
   this.getAllItems = () => {
     return [...this._items];
   };
 
+  // Retrieve a single item by id.
   this.getItem = itemId => {
-    let item = this.getAllItems().filter(it => it.id === itemId);
-    if (item.length > 0) {
-      return item[0];
+    let items = this.getAllItems();
+    let itemIndex = items.map(item => item.id).indexOf(itemId);
+    if (itemId !== -1) {
+      return items[itemIndex];
     } else {
       throw new Error("There isn't an item with the passed id.");
     }
   };
 
+  // Add item to the cart.
   this.addItem = item => {
     let items = this.getAllItems();
     let isItemInCart = items.some(it => it.id === item.id);
@@ -42,17 +48,12 @@ function newCart(storage, initItems) {
     }
   };
 
+  // Remove item by id.
   this.removeItem = itemId => {
     let items = this.getAllItems();
     // let itemIndex = items.filter(it => it.id === itemId).map(it => it.id);
-    let itemIndex = items.reduce((acc, it, index) => {
-      if (it.id === itemId) {
-        acc += index;
-      }
-      return acc;
-    }, 0);
-    if (itemIndex !== undefined) {
-      console.log(items.slice(0, itemIndex));
+    let itemIndex = items.map(item => item.id).indexOf(itemId);
+    if (itemIndex !== -1) {
       let updatedCart = [
         ...items.slice(0, itemIndex),
         ...items.slice(itemIndex + 1),
@@ -63,24 +64,22 @@ function newCart(storage, initItems) {
     }
   };
 
+  // Update an item.
   this.updateItem = (itemId, itemData) => {
     let items = this.getAllItems();
     if (typeof itemData === 'object') {
       if (itemData.hasOwnProperty('id')) {
-        throw new Error('Please passed a data object without an id.');
+        throw new Error('Please pass a data object without id.');
       }
 
+      let itemToUpdateIndex = items.map(item => item.id).indexOf(itemId);
       let propertiesToUpdate = Object.keys(itemData);
-      let itemToUpdate = items.filter(it => it.id === itemId);
 
-      if (itemToUpdate.length > 0) {
-        let [item] = itemToUpdate;
-
+      if (itemToUpdateIndex !== -1) {
         propertiesToUpdate.forEach(prop => {
-          if (item[prop] !== undefined) {
-            item[prop] = itemData[prop];
-          }
+          items[itemToUpdateIndex][prop] = itemData[prop];
         });
+        this.saveItems(items);
       } else {
         throw new Error("Couldn't find an item with this id.");
       }
@@ -89,13 +88,14 @@ function newCart(storage, initItems) {
     }
   };
 
+  // Clear the cart
   this.clear = () => {
     this.saveItems([]);
     storage.clear();
   };
 
   /*
-   * If no storage connector is passed default to localStorage.
+   * If no storage connector is passed on new, default to localStorage.
    */
   if (storage === undefined || storage === null) {
     storage = {
@@ -113,7 +113,7 @@ function newCart(storage, initItems) {
   }
 
   /*
-   * Initialize cart with passed items on initialization.
+   * Initialize cart with items inside passed as parameters.
    */
   if (initItems !== undefined || initItems !== null) {
     this.saveItems(initItems);
@@ -122,140 +122,4 @@ function newCart(storage, initItems) {
   }
 }
 
-const Cart = function(storage) {
-  return {
-    /*
-     * Properties
-     */
-    // Private property that holds the cart items.
-    _items: [],
-    /*
-     * Methods
-     */
-    // Getter of the cart items.
-    getItems() {
-      return this._items;
-    },
-    // Setter of the cart items.
-    setItems(cartItems = []) {
-      if (Array.isArray(cartItems)) {
-        this._items = cartItems;
-        storage.save(this._items);
-        publish('updated-counter', { cartCount: this._items.length });
-      }
-    },
-    // Initialize the new cart object.
-    init() {
-      // Gets the cartItems from the storage and stored them into the instance's private property.
-      let storedItems = storage.get();
-      this.setItems(storedItems);
-
-      // we have to set a global variable called cart pointing to out cart instance so we can let the preact
-      // component use the exact same instance and the same _items array to keep track
-      // of the current cart session.
-      window.cart = this;
-
-      // update the cart counter with the current count
-      document.querySelector(
-        '.cart-counter'
-      ).textContent = this.getItems().length;
-    },
-    // Add an item to the cart.
-    addItem(itemObject) {
-      // Array from because we have to avoid direct mutation of our
-      // cart items array.
-      let items = Array.from(this.getItems());
-
-      if (!idExistsInArray(items, itemObject.id)) {
-        items.push(itemObject);
-        this.setItems(items);
-        publish('success', {
-          type: 'success',
-          message: itemObject.name + ' has been succesfully added to the cart.',
-        });
-      } else {
-        publish('error', {
-          type: 'error',
-          message: itemObject.name + ' is already in the cart.',
-        });
-        throw new Error('item already exists');
-      }
-    },
-    removeItem(itemId) {
-      // Array from because we have to avoid direct mutation of our
-      // cart items array.
-      let items = Array.from(this.getItems());
-      let itemIndex, itemName;
-      for (let i = 0; i < items.length; i++) {
-        if (items[i].id === itemId) {
-          itemIndex = i;
-          itemName = items[i].name;
-        }
-      }
-      if (itemIndex !== undefined) {
-        items.splice(itemIndex, 1);
-        console.log('items before deleting', this.getItems());
-        this.setItems(items);
-        console.log('items after deleting', this.getItems());
-        publish('success', {
-          type: 'success',
-          message: itemName + ' has been removed.',
-        });
-      } else {
-        return new Error("there isn't any item with that id ");
-      }
-    },
-    clear() {
-      this.setItems([]);
-      storage.clear();
-      publish('success', {
-        type: 'warning',
-        message: 'Cart has been cleared out.',
-      });
-    },
-    getItem(itemId) {
-      for (let i = 0; i < this.currentItems.length; i++) {
-        if (this.currentItems[i].id === itemId) {
-          return this.currentItems[i];
-        } else {
-          return new Error('there isnt any item with that id');
-        }
-      }
-    },
-    updateItem(itemId, data) {
-      let currentItems = this.getItems();
-      if (typeof data === 'object') {
-        if (data.hasOwnProperty('id')) {
-          throw new Error("there shoul'd be a id property in the data object");
-          // delete data.id;
-        }
-        // Check if there's any other property existing there.
-        if (
-          data.name !== undefined ||
-          data.slug !== undefined ||
-          data.type !== undefined
-        ) {
-          for (let i = 0; i < currentItems.length; i++) {
-            // Find the actual item.
-            if (currentItems[i].id === itemId) {
-              // Overwrite the item data.
-              Object.assign(currentItems[i], data);
-              // Save the data to the global cart.
-              this.setItems(currentItems);
-              publish('success', {
-                type: 'success',
-                message: currentItems[id].name + ' has been updated',
-              });
-            }
-          }
-        } else {
-          throw new Error('not item found with that id');
-        }
-      } else {
-        throw new Error('the data passed is not an object');
-      }
-    },
-  };
-};
-
-module.exports = { Cart, newCart };
+module.exports = Cart;
